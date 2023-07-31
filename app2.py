afe11b0d8af7c2bed77d3e25e2b78ede0cefef76
VictorDedalus <lambertvictor24@gmail.com>
2023-07-31 23:05:51 +0200

    trying to upload the data to my heroku app since the csv files are very small

--- app2.py
+++ app2.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 
 @app.route('/api/users')
 def get_list_of_users():
-    app_test = pd.read_csv('data/application_test_cleaned.csv').drop(columns='Unnamed: 0')
+    app_test = pd.read_csv('application_test_cleaned.csv').drop(columns='Unnamed: 0')
     list_users = list(app_test['SK_ID_CURR'])
 
     return jsonify({'users': list_users})
 
 @app.route('/api/user/<user_id>')
 def get_specific_data(user_id):
-    app_test = pd.read_csv('data/application_test_cleaned.csv').drop(columns='Unnamed: 0')
-    app_test_scaled = pd.read_csv('data/application_test_scaled.csv').drop(columns='Unnamed: 0')
+    app_test = pd.read_csv('application_test_cleaned.csv').drop(columns='Unnamed: 0')
+    app_test_scaled = pd.read_csv('application_test_scaled.csv').drop(columns='Unnamed: 0')
     filename = "scoring_model.pkl"
     model = pickle.load(open("modele/" + filename, 'rb'))
     result = model.predict_proba(app_test_scaled)[:,0]
@@ -27,7 +27,7 @@ def get_specific_data(user_id):
 
 @app.route('/api/shap_features')
 def get_shap_features():
-    shap_features = pd.read_csv('data/shap_features.csv').drop(columns='Unnamed: 0').to_json(orient="columns")
+    shap_features = pd.read_csv('shap_features.csv').drop(columns='Unnamed: 0').to_json(orient="columns")
     return shap_features
 
 

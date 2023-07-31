3d6f2297399ac3c52dd845d0af4a53223a125051
VictorDedalus <lambertvictor24@gmail.com>
2023-07-31 22:20:23 +0200

    reduced the size of df + added requirements for deployment

--- app2.py
+++ app2.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 
 @app.route('/api/users')
 def get_list_of_users():
-    app_test = pd.read_csv('data/application_test_cleaned.csv').drop(columns='Unnamed: 0').iloc[0:100]
+    app_test = pd.read_csv('data/application_test_cleaned.csv').drop(columns='Unnamed: 0')
     list_users = list(app_test['SK_ID_CURR'])
 
     return jsonify({'users': list_users})
 
 @app.route('/api/user/<user_id>')
 def get_specific_data(user_id):
-    app_test = pd.read_csv('data/application_test_cleaned.csv').drop(columns='Unnamed: 0').iloc[0:100]
-    app_test_scaled = pd.read_csv('data/application_test_scaled.csv').drop(columns='Unnamed: 0').iloc[0:100]
+    app_test = pd.read_csv('data/application_test_cleaned.csv').drop(columns='Unnamed: 0')
+    app_test_scaled = pd.read_csv('data/application_test_scaled.csv').drop(columns='Unnamed: 0')
     filename = "scoring_model.pkl"
     model = pickle.load(open("modele/" + filename, 'rb'))
     result = model.predict_proba(app_test_scaled)[:,0]
